3fb899cf794f1f7c75cb1e051b7f0babd4dacc59
hatobanono <chris00iq@gmail.com>
2021-07-29 23:44:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,31 +29,62 @@ client.on('message', msg => {
         const cmd = msg.content.substring(prefix.length).split(' '); //以空白分割前綴以後的字串
 
         if (cmd.length == 1) {
-          if (encommandlist.indexOf(encodeURIComponent(cmd[0])) >= 0) {
-            db.get(encodeURIComponent(cmd[0])).then(response => {
-              msg.channel.send(response);
-              console.log('回覆' + response);
-            });
-          };
-        };
+          switch (cmd[0]) {
+            case '阿夸占卜':
+              db.get("randomaqualist").then(randomaquas => {
+                msg.channel.send(randomaquas[Math.floor(Math.random() * randomaquas.length)]);
+              });
+              break;
 
-        if (cmd.length == 2, cmd[0] == 'delcommand') {
-          if (encommandlist.indexOf(encodeURIComponent(cmd[1])) >= 0) {
-            db.delete(encodeURIComponent(cmd[1]));
-            msg.channel.send('刪除 ' + cmd[1] + ' 指令');
-          } else {
-            msg.channel.send('找不到 ' + cmd[1] + ' 指令');
+            case '吃啥':
+              db.get("randomdinnerlist").then(randomdinners => {
+                msg.channel.send(randomdinners[Math.floor(Math.random() * randomdinners.length)]);
+              });
+              break;
+
+            case 'peko占卜':
+              db.get("randompekoralist").then(randompekoras => {
+                msg.channel.send(randompekoras[Math.floor(Math.random() * randompekoras.length)]);
+              });
+              break;
+
+            default:
+              if (encommandlist.indexOf(encodeURIComponent(cmd[0])) >= 0) {
+                db.get(encodeURIComponent(cmd[0])).then(response => {
+                  msg.channel.send(response);
+                  console.log('回覆' + response);
+                });
+              };
           }
         };
 
-        if (cmd.length >= 3, cmd[0] == 'addcommand') {
-          if (encommandlist.indexOf(encodeURIComponent(cmd[1])) >= 0) {
-            msg.channel.send('已經有 ' + cmd[1] + ' 指令');
-          } else {
-            const text = msg.content.slice(cmd[0].length + cmd[1].length + 3);
-            console.log('內容 ' + text);
-            db.set(cmd[1], text);
-            msg.channel.send('增加 ' + cmd[1] + ' 指令');
+        if (cmd.length == 2) {
+          if (cmd[0] == 'delcommand') {
+            if (encommandlist.indexOf(encodeURIComponent(cmd[1])) >= 0) {
+              db.delete(encodeURIComponent(cmd[1]));
+              msg.channel.send('刪除 ' + cmd[1] + ' 指令');
+            } else {
+              msg.channel.send('找不到 ' + cmd[1] + ' 指令');
+            }
+          }
+        };
+
+        if (cmd.length >= 3) {
+          switch (cmd[0]) {
+            case 'addcommand':
+              if (encommandlist.indexOf(encodeURIComponent(cmd[1])) >= 0) {
+                msg.channel.send('已經有 ' + cmd[1] + ' 指令');
+              } else {
+                const text = msg.content.slice(cmd[0].length + cmd[1].length + 3);
+                console.log('內容 ' + text);
+                db.set(cmd[1], text);
+                msg.channel.send('增加 ' + cmd[1] + ' 指令');
+              }
+              break;
+            case 'addrandomlist':
+              const randomlist = cmd.slice(2)
+              db.set(cmd[1], randomlist);
+              break;
           }
         }
       });
