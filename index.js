7cd50da02dca2e99756f75267e47ee6ef8b6f6aa
hatobanono <81277283+hatobanono@users.noreply.github.com>
2022-12-05 23:43:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const Discord = require("discord.js")
-const Database = require("@replit/database")
-const keepAlive = require("./server")
+const express = require('express');
+const app = express();
 
-const db = new Database()
-const client = new Discord.Client()
+app.get('/', (req, res) => {
+  res.send('Hello Express app!');
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const Database = require('@replit/database');
+const db = new Database();
+const token = process.env['TOKEN']
+
+client.login(token);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -16,83 +28,97 @@ client.on('message', msg => {
     if (!msg.member.user) return; //幫bot值多拉一層，判斷上層物件是否存在
     if (msg.member.user.bot) return; //訊息內bot值為正 = 此消息為bot發送
   } catch (err) {
+    console.log(err);
     return;
   }
   //字串分析
   try {
-    const prefix = '!' //前綴符號定義
-    if (msg.content.substring(0, prefix.length) === prefix) //如果訊息的開頭~前綴字長度的訊息 = 前綴字
-    {
+    const prefix = '!'; //前綴符號定義
+    if (msg.content.substring(0, prefix.length) === prefix) {
       db.list().then(commandlist => {
         const encommandlist = encodeURIComponent(commandlist);
-        console.log('列表' + commandlist);
+        //如果訊息的開頭~前綴字長度的訊息 = 前綴字
         const cmd = msg.content.substring(prefix.length).split(' '); //以空白分割前綴以後的字串
-
         if (cmd.length == 1) {
           switch (cmd[0]) {
             case '阿夸占卜':
-              db.get("randomaqualist").then(randomaquas => {
-                msg.channel.send(randomaquas[Math.floor(Math.random() * randomaquas.length)]);
-              });
+              GetRandomAqua();
               break;
-
-            case '吃啥':
-              db.get("randomdinnerlist").then(randomdinners => {
-                msg.channel.send(randomdinners[Math.floor(Math.random() * randomdinners.length)]);
-              });
-              break;
-
             case 'peko占卜':
-              db.get("randompekoralist").then(randompekoras => {
-                msg.channel.send(randompekoras[Math.floor(Math.random() * randompekoras.length)]);
-              });
+              GetRandomPeko();
               break;
-
+            case '吃啥':
+              GetRandomdinner();
+              break;
             default:
-              if (encommandlist.indexOf(encodeURIComponent(cmd[0])) >= 0) {
-                db.get(encodeURIComponent(cmd[0])).then(response => {
-                  msg.channel.send(response);
-                  console.log('回覆' + response);
-                });
-              };
+              if (IsExist(cmd[0])) {
+                GetResponse(cmd[0]);
+              }
           }
-        };
-
-        if (cmd.length == 2) {
-          if (cmd[0] == 'delcommand') {
-            if (encommandlist.indexOf(encodeURIComponent(cmd[1])) >= 0) {
-              db.delete(encodeURIComponent(cmd[1]));
-              msg.channel.send('刪除 ' + cmd[1] + ' 指令');
+        } else if (cmd.length == 2) {
+          if (cmd[0] === 'delcommand') {
+            if (IsExist(cmd[1])) {
+              DeleteCommand(cmd[1]);
             } else {
-              msg.channel.send('找不到 ' + cmd[1] + ' 指令');
+              msg.channel.send(`找不到 ${cmd[1]} 指令`);
             }
           }
-        };
-
-        if (cmd.length >= 3) {
+        } else if (cmd.length >= 3) {
           switch (cmd[0]) {
             case 'addcommand':
-              if (encommandlist.indexOf(encodeURIComponent(cmd[1])) >= 0) {
-                msg.channel.send('已經有 ' + cmd[1] + ' 指令');
+              if (IsExist(cmd[1])) {
+                msg.channel.send(`已經有 ${cmd[1]} 指令`);
               } else {
-                const text = msg.content.slice(cmd[0].length + cmd[1].length + 3);
-                console.log('內容 ' + text);
+                let text = msg.content.slice(cmd[0].length + cmd[1].length + 3);
                 db.set(cmd[1], text);
-                msg.channel.send('增加 ' + cmd[1] + ' 指令');
+                msg.channel.send(`增加 ${cmd[1]} 指令`);
               }
               break;
             case 'addrandomlist':
-              const randomlist = cmd.slice(2)
+              let randomlist = cmd.slice(2);
               db.set(cmd[1], randomlist);
+              msg.channel.send('新增完成');
               break;
           }
         }
-      });
+        function IsExist(key) {
+          return encommandlist.indexOf(encodeURIComponent(key)) >= 0;
+        }
+        function DeleteCommand(key) {
+          db.delete(encodeURIComponent(key)).then(() => {
+            msg.channel.send(`刪除 ${cmd[1]} 指令`);
+          });
+        }
+      })
     }
   } catch (err) {
-    console.log('OnMessageError', err);
+    console.log(err);
+    return;
+  }
+  function GetResponse(key) {
+    db.get(encodeURIComponent(key)).then(response => {
+      msg.channel.send(response);
+    });
+  }
+  function GetRandomAqua() {
+    db.get('randomaqualist').then(randomaquas => {
+      msg.channel.send(
+        randomaquas[Math.floor(Math.random() * randomaquas.length)]
+      );
+    });
+  }
+  function GetRandomPeko() {
+    db.get('randompekoralist').then(randompekoras => {
+      msg.channel.send(
+        randompekoras[Math.floor(Math.random() * randompekoras.length)]
+      );
+    });
+  }
+  function GetRandomdinner() {
+    db.get('randomdinnerlist').then(randomdinners => {
+      msg.channel.send(
+        randomdinners[Math.floor(Math.random() * randomdinners.length)]
+      );
+    });
   }
 });
-
-keepAlive()
-client.login(process.env.TOKEN);
